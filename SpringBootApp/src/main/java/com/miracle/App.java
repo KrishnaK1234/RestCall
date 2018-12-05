63f66d917f9f34ece3c2fa7ea33b3540782dd411
kkota <kkota@GUEST252.corp.miraclesoft.com>
2018-12-05 18:25:31 +0530

    SpringBootApp

---
+++ SpringBootApp/src/main/java/com/miracle/App.java
@@ -0,0 +1,19 @@
+package com.miracle;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+//		ApplicationContext ctx = SpringApplication.run(App.class, args);
+		SpringApplication.run(App.class, args);
+		/*
+		 * String[] beanNames = ctx.getBeanDefinitionNames(); Arrays.sort(beanNames);
+		 * for (String beanName : beanNames) { System.out.println(beanName); }
+		 */
+	}
+}
